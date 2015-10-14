1ce60efd23fa2fe614352c3ccbce44524a067d74
ashtondavvo <ashton.d173@live.com>
2015-10-14 21:25:38 +1300

    menu functionality

--- Assets/Scripts/menu/menuScript.cs
+++ Assets/Scripts/menu/menuScript.cs
@@ -4,17 +4,28 @@ using System.Collections;
 
 public class menuScript : MonoBehaviour 
 {
+	public Canvas controlMenu;
 	public Canvas quitMenu;
 	public Button startText;
+	public Button controls;
 	public Button exitText;
+
+	public Image playButton;
+	public Image controlButton;
+	public Image exitButton;
+	public Image logo;
 	
 	void Start ()
 		
 	{
 		quitMenu = quitMenu.GetComponent<Canvas>();
 		startText = startText.GetComponent<Button> ();
+		controls = controls.GetComponent<Button> ();
 		exitText = exitText.GetComponent<Button> ();
+		controlMenu.enabled = false;
 		quitMenu.enabled = false;
+
+
 		
 	}
 	
@@ -23,17 +34,54 @@ public class menuScript : MonoBehaviour
 	{
 		quitMenu.enabled = true; //enable the Quit menu when we click the Exit button
 		startText.enabled = false; //then disable the Play and Exit buttons so they cannot be clicked
+		controls.enabled = false;
 		exitText.enabled = false;
 		
 	}
 	
+	public void ControlsPress()
+	
+	{
+		controlMenu.enabled = true;
+		startText.enabled = false; //then disable the Play and Exit buttons so they cannot be clicked
+		controls.enabled = false;
+		exitText.enabled = false;
+
+		Color color = playButton.color;
+		color.a = 0;
+		playButton.color = color;
+		Color colorA = controlButton.color;
+		colorA.a = 0;
+		controlButton.color = colorA;
+		Color colorB = exitButton.color;
+		colorB.a = 0;
+		exitButton.color = colorB;
+		Color colorC = logo.color;
+		colorC.a = 0;
+		logo.color = colorC;
+	}
+
 	public void NoPress() //this function will be used for our "NO" button in our Quit Menu
 		
 	{
+		controlMenu.enabled = false;
 		quitMenu.enabled = false; //we'll disable the quit menu, meaning it won't be visible anymore
 		startText.enabled = true; //enable the Play and Exit buttons again so they can be clicked
+		controls.enabled = true;
 		exitText.enabled = true;
-		
+
+		Color color = playButton.color;
+		color.a = 1;
+		playButton.color = color;
+		Color colorA = controlButton.color;
+		colorA.a = 1;
+		controlButton.color = colorA;
+		Color colorB = exitButton.color;
+		colorB.a = 1;
+		exitButton.color = colorB;
+		Color colorC = logo.color;
+		colorC.a = 1;
+		logo.color = colorC;
 	}
 	
 	public void StartLevel () //this function will be used on our Play button
